fc7527e865f56e2ebe39b03cf3baac10a1e59ebc
PrelawAverage <bryanguzman859@gmail.com>
2023-04-15 22:54:08 -0400

    Changed our mappings to us Parchment, an open-source mapping community where everyone is allowed to contribute, basically quick updates.

---
+++ src/main/java/com/lucky/battletowers/entity/custom/BattleTowerGolemEntity.java
@@ -0,0 +1,11 @@
+package com.lucky.battletowers.entity.custom;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+
+public class BattleTowerGolemEntity extends Monster {
+    protected BattleTowerGolemEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+}
